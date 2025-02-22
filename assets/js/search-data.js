6e0738bd6d36e072a7bd02fd88002f94758fc7b2
debrinaangelica <debrinaangelica@gmail.com>
2025-02-22 00:27:33 +0000

    Deploying to gh-pages from @ debrinaangelica/debrinaangelica.github.io@f612c51951175c86d6ce5f0587c8183cc6d90026 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -379,9 +379,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-acrylic-coffee-cup-sleeve",
+          title: 'acrylic coffee cup sleeve',
+          description: "24672 ps1",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
