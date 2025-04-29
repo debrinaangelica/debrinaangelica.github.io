229f0cf9540c88f67bac2a89cfc9707c435da427
debrinaangelica <debrinaangelica@gmail.com>
2025-04-29 22:44:07 +0000

    Deploying to gh-pages from @ debrinaangelica/debrinaangelica.github.io@c4accbea29b81c84d2a2342840fbea9ee25a202b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -399,6 +399,21 @@ ninja.data = [{
           description: "24672 ps3a",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-final-project",
+          title: 'final project',
+          description: "24672 ps7",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
             },},{
         id: 'social-email',
         title: 'email',
